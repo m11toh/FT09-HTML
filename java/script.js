bb445b5a9f27b100e6027209bd29cd03ed726915
m11toh <martohmashah@gmail.com>
2024-03-15 13:56:47 +0300

    Java

---
+++ java/script.js
@@ -0,0 +1,42 @@
+// functions
+
+function addGrades(asgn1, asgn2) {
+    //work space
+    let totalMarks = asgn1 + asgn2
+    console.log(totalMarks)
+    //return
+    return totalMarks -2
+}
+
+console.log("Martin: ", addGrades(7,5))
+console.log("Ramsey: ", addGrades(5,5))
+console.log("Rasey: ", addGrades(6,5))
+
+//Anatomy
+//Function keyword
+//function name
+// parameters
+
+//function body
+//workspace
+//return function
+
+// function call
+    //function arguments
+
+
+
+// CONDITIONAL STATEMENTS
+
+let age = 40
+
+if (age < 17){
+    console.log("Hello! Welcome to the system")
+
+}else if(age > 32){
+    console.log("You are too old for the system")
+}else{
+    console.log("Hello! Welcome to the system")
+}
+
+
